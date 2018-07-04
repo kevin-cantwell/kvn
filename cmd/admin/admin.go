56dfb8e9d395e7a2c7acdd3094601a6442c1fe1e
Kevin Cantwell <kevin.cantwell@gmail.com>
2018-07-05 11:28:14 +1200

    Revert "Revert "Revert "touch"""
    This reverts commit 05d16990cef1c1f93f9ba1ef7b140129eba272fe.

--- cmd/admin/admin.go
+++ cmd/admin/admin.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "admin dyno: %s", os.Getenv("DYNO"))
+		fmt.Fprintf(w, "DYNO: %s", os.Getenv("DYNO"))
 	})
 	log.Println("http://localhost:" + os.Getenv("PORT"))
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
